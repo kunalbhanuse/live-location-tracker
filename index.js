56b55884e97003aca14ea882d0114ecc10e5341d
kunal bhanuse <kunalbhanuse07@gmail.com>
2026-05-03 16:22:43 +0530

    socket connected and live location emit success

--- index.js
+++ index.js
@@ -8,10 +8,21 @@ async function main() {
   const app = express();
   const server = http.createServer(app);
   const io = new Server();
-
-  io.attach(server);
   app.use(express.json());
   app.use(express.static(path.resolve("./public")));
+
+  io.attach(server);
+  io.on("connection", (socket) => {
+    console.log(`socket :- ${socket.id} : Connected success ....`);
+    socket.on("client:location:update", (locationData) => {
+      const { latitude, longitude } = locationData;
+      console.log(
+        `socket :- ${socket.id} : client:location:update`,
+        locationData,
+      );
+    });
+  });
+
   app.get("/health", (req, res) => {
     return res.json({ healthy: "ok" });
   });
